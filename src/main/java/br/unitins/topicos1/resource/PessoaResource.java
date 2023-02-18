1287b4b9f11bfb631531c117a34fc8b91a43e36b
Janio <janiojunior@gmail.com>
2023-02-18 10:40:50 -0300

    alula 3

--- src/main/java/br/unitins/topicos1/resource/PessoaResource.java
+++ src/main/java/br/unitins/topicos1/resource/PessoaResource.java
@@ -6,7 +6,9 @@ import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -33,6 +35,21 @@ public class PessoaResource {
         pessoa.persist();
 
         return pessoa;
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Pessoa uptadte(@PathParam("id") Long id, Pessoa pessoa) {
+
+        Pessoa entity = Pessoa.findById(id);
+
+        entity.setCpf(pessoa.getCpf());
+        entity.setNome(pessoa.getNome());
+
+        return entity;
     }
     
 }
