fecd8445daf7d494df5418caeae27b7a161b7aa7
Luahr13 <73291538+Luahr13@users.noreply.github.com>
2023-03-24 19:28:13 -0300

    updade
    Validation

--- src/main/java/br/unitins/topicos1/service/MunicipioServiceImpl.java
+++ src/main/java/br/unitins/topicos1/service/MunicipioServiceImpl.java
@@ -9,6 +9,7 @@ import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
 import javax.validation.Validator;
 import javax.ws.rs.NotFoundException;
 
